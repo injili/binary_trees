553ebbeeb2d4e7dbc5dc322b669eb818285ed111
injili <nyarekigospel@gmail.com>
2023-08-02 14:26:55 -0700

    0. New node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -13,11 +13,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newnode;
 
-	(void) (*newnode);
-	(void) (value);
-
 	newnode = malloc(sizeof(binary_tree_t));
-	if (newnode == NULL)
+	if (!newnode)
 		return (NULL);
 
 	newnode->n = value;
